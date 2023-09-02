09df31353acb3e09d400392be0a2370a1f2b5c81
zubizhenya <e.zubilin@yandex.ru>
2023-09-02 13:37:11 +0700

    part2

--- symplesite/bboard/models.py
+++ symplesite/bboard/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core import validators
+from django.core.exceptions import ValidationError
 
 
 class Rubric(models.Model):
@@ -19,12 +21,19 @@ class Bb(models.Model):
         __empty__ = 'Выберите тип публикуемого обьявления'
 
     kind = models.CharField(max_length=10, choices=Kinds.choices, default=Kinds.SELL)
-    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика', related_name='entries')
     title = models.CharField(max_length=50, verbose_name='Товар')
     content = models.TextField(null=True, blank=True, verbose_name='Описание')
     price = models.FloatField(null=True, blank=True, verbose_name='Цена')
     published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Опубликовано')
-
+    def clean(self):
+        errors = {}
+        if not self.content:
+            errors['content'] = ValidationError('Укажите описание продаваемого товара')
+        if self.price and self.price < 0:
+            errors['price'] = ValidationError('Укажите неотрицательные цены')
+        if errors:
+            raise ValidationError(errors)
 
     class Meta:
         verbose_name_plural = 'Обьявления'

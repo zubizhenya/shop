01c5c8d6554b13016b8ba26a97d9e5885fdd0ea4
zubizhenya <e.zubilin@yandex.ru>
2023-09-04 13:16:57 +0700

    part3

--- symplesite/bboard/urls.py
+++ symplesite/bboard/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, by_rubric, BbCreateView
+from .views import index, BbByRybricView, BbCreateView, BbDetailView
 
 urlpatterns = [
     path('add/', BbCreateView.as_view(), name='add'),
-    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('<int:rubric_id>/', BbByRybricView.as_view(), name='by_rubric'),
+    path('detail/<int:pk>/', BbDetailView.as_view(), name='detail'),
     path('', index, name='index'),
 ]
--- symplesite/bboard/views.py
+++ symplesite/bboard/views.py
@@ -3,16 +3,27 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Bb, Rubric
 from django.views.generic.edit import CreateView
+from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 from .forms import BbForm
 from django.urls import reverse_lazy
 
-def by_rubric(request, rubric_id):
-    bbs = Bb.objects.filter(rubric=rubric_id)
-    rubrics = Rubric.objects.all()
-    current_rubric = Rubric.objects.get(pk=rubric_id)
-    context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
-    return render(request, 'bboard/by_rubric.html', context)
+class BbByRybricView(TemplateView):
+    template_name = 'bboard/by_rubric.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['bbs'] = Bb.objects.filter(rubric=context['rubric_id'])
+        context['rubrics'] = Bb.objects.all()
+        context['current_rubric'] = Rubric.objects.get(pk=context['rubric_id'])
+        return context
+
+class BbDetailView(DetailView):
+    model = Bb
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
 def index(request):
     bbs = Bb.objects.all()
     rubrics = Rubric.objects.all()
